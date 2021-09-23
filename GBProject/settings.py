45c1afc294474d574a1e363c6d7e42f17fe968b0
avnyaswanth <yaswanthalapati20@gmail.com>
2021-09-23 11:39:52 +0530

    static root added

--- GBProject/settings.py
+++ GBProject/settings.py
@@ -111,7 +111,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
